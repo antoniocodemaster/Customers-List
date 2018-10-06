8df142b3c80abc8dbadce6cb1faebedc0b0e8437
antoniofloresonline <wanllyf@gmail.com>
2018-10-05 23:14:33 -0600

    basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <template>
-  <div id="app">
-    <h1>Hola Mundo</h1>
+  <div id="app" class="app">
+    <clnav/>
+    <div class="container appcontainer">
+      <div class="row">
+        <div class="list col-md-4">
+          <customerslist/>
+        </div>
+        <div class="details col-md-8">
+          <customersinfo/>
+        </div>
+      </div>
+    </div>
+    <clfooter/>
   </div>
 </template>
 
 <script>
+
+// Layout
+import clnav from './components/layout/clnav.vue'
+import clfooter from './components/layout/clfooter.vue'
+
+import customerslist from './components/customerslist.vue'
+import customersinfo from './components/customersinfo.vue'
+
 export default {
   name: 'app',
+  components:{
+    clfooter,
+    clnav,
+    customerslist,
+    customersinfo
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
--- src/components/layout/clfooter.vue
+++ src/components/layout/clfooter.vue
@@ -1,6 +1,9 @@
 <template>
-	<footer>
-		I am the footer
+	<footer class="container" >
+		<p>Lorem Ipsum</p>
+		<button class="btn btn-primary btn-sm"  >Add New</button>
+		<button class="btn btn-warning btn-sm"  >Edit</button>
+		<button class="btn btn-danger btn-sm" >Remove</button>
 	</footer>
 </template>
 
