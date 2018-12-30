c7729ed1f7bac2cd9c8db60a39396536cca989a1
antoniofloresonline <wanllyf@gmail.com>
2018-12-30 09:47:42 -0600

    font size and borders

--- src/components/layout/clnav.vue
+++ src/components/layout/clnav.vue
@@ -3,7 +3,7 @@
   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <a class="navbar-brand" href="#">Customer's List, by Antonio Flores</a>
+  <h1>Customer's List, by Antonio Flores</h1>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
